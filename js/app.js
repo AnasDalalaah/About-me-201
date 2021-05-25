e673d0c2f07b94fbede5455248f24d535c806ea4
AnasDalalaah <anasfuad232@gmail.com>
2021-05-25 21:02:21 +0300

    editing JS code

--- js/app.js
+++ js/app.js
@@ -1,88 +1,88 @@
 'use strict';
 
-const GA='Guten Abend'
+const GA = 'Guten Abend';
 
 alert(GA);
 
-let yourName=prompt('what is your name?');
-        console.log(yourName);
+let yourName = prompt('what is your name?');
+console.log(yourName);
 
-        let age=prompt('what is your age?');
+let age = prompt('what is your age?');
 
-        console.log(age);
+console.log(age);
 
-        alert('Your name and age are written this way in the German language, Mein Name ist '   + yourName+ '  Ich bin '+ age +' Jahre alt');
-
-
-        /*let book=prompt('What do you want to read fields of environment or economy?');
-
-        console.log(book);
-        
-        book=book.toLowerCase(); 
-
-        console.log('Correct Case',book);
-        
-        if(book=== 'environment'){
-            console.log('Climate change');
-            alert('Climate change');
-        }else if(book==='economy'){
-            console.log('Stock market');
-            alert('Stock market');
-        }else{
-           console.log('Please select economy or environment');
-           alert('Please select economy or environment');
-        }
-        
-        let SMP=prompt('What is your favorite social media platform (LINKEDIN,TWITTER)?');
-        
-        console.log(SMP);
-
-        SMP=SMP.toUpperCase();
-
-        console.log('Correct Case',SMP);
+alert('Your name and age are written this way in the German language, Mein Name ist ' + yourName + '  Ich bin ' + age + ' Jahre alt');
 
 
-        if(SMP=== 'LINKEDIN'){
-            console.log('The most professional Socialmedia platform')
-            alert('The most professional Socialmedia platform')
-        }
-        else if(SMP==='TWITTER'){
-            console.log('The most reliable Socialmedia platform');
-            alert('The most reliable Socialmedia platform')
+/*let book=prompt('What do you want to read fields of environment or economy?');
 
-        }else{
+console.log(book);
 
-           console.log('Please check the major entry next time');
-           alert('Please check the major entry next time');
-        }
+book=book.toLowerCase();
 
-        let music=confirm('Do you like music?');
+console.log('Correct Case',book);
+
+if(book=== 'environment'){
+    console.log('Climate change');
+    alert('Climate change');
+}else if(book==='economy'){
+    console.log('Stock market');
+    alert('Stock market');
+}else{
+   console.log('Please select economy or environment');
+   alert('Please select economy or environment');
+}
+
+let SMP=prompt('What is your favorite social media platform (LINKEDIN,TWITTER)?');
+
+console.log(SMP);
+
+SMP=SMP.toUpperCase();
+
+console.log('Correct Case',SMP);
+
+
+if(SMP=== 'LINKEDIN'){
+    console.log('The most professional Socialmedia platform')
+    alert('The most professional Socialmedia platform')
+}
+else if(SMP==='TWITTER'){
+    console.log('The most reliable Socialmedia platform');
+    alert('The most reliable Socialmedia platform')
+
+}else{
+
+   console.log('Please check the major entry next time');
+   alert('Please check the major entry next time');
+}
+
+let music=confirm('Do you like music?');
 
 if (music===true){
-     console.log('Calms nerves');
-     alert('Calms nerves')
- }else{     console.log('Try to hear it');
- alert('Try to hear it')
+console.log('Calms nerves');
+alert('Calms nerves')
+}else{     console.log('Try to hear it');
+alert('Try to hear it')
 }
 
 let Party = prompt('Which British party are you voting for(conservative party,labour party)?');
 console.log(Party);
 
 switch (Party.toLowerCase()) {
-    case 'conservative party':
-        console.log('It is the ruling party');
-        alert('It is the ruling party');
-        break;
+case 'conservative party':
+console.log('It is the ruling party');
+alert('It is the ruling party');
+break;
 
-    case 'labour party':
-        console.log('It is the opposition party');
-        alert( 'It is the opposition party');
-        break;
+case 'labour party':
+console.log('It is the opposition party');
+alert( 'It is the opposition party');
+break;
 
-    default:
-        console.log('You should select one of them');
-        alert('You should select one of them');
-        break;
+default:
+console.log('You should select one of them');
+alert('You should select one of them');
+break;
 }
 
 let A= prompt('What is the best technical university (MIT, California-BERKELEY,STANFORD)?');
@@ -90,114 +90,120 @@ let A= prompt('What is the best technical university (MIT, California-BERKELEY,S
 console.log(A);
 
 switch (A.toUpperCase()) {
-    case 'MIT':
-        console.log('It is on the east coast');
-        alert('It is on the east coast');
-        break;
+case 'MIT':
+console.log('It is on the east coast');
+alert('It is on the east coast');
+break;
 
-    case 'California-BERKELEY':   
-    case 'STANFORD':
-        console.log('It is on the west coast');
-        alert('It is on the west coast');
+case 'California-BERKELEY':
+case 'STANFORD':
+console.log('It is on the west coast');
+alert('It is on the west coast');
 
-    break;
+break;
 
-    default:
-        console.log('You should select one of them');
-        alert('You should select one of them');
-        break;
+default:
+console.log('You should select one of them');
+alert('You should select one of them');
+break;
 }*/
 
-let book=prompt('Do you like books? (yes/no)')
+let book = prompt('Do you like books? (yes/no)');
 
-book=book.toLocaleLowerCase();
-if (book==='yes') {
-    //console.log('Good Choice');
-    alert('Good Choice');
-    
-}else if(book==='no'){
-    //console.log('You need a second chance');
-    alert('You need a second chance');
+book = book.toLocaleLowerCase();
 
-}else{
-    //console.log('Does not exist');
-    alert('Your answer not exist');
+if (book === 'yes' || book==='y' ) {
+  //console.log('Good Choice');
+  alert('Good Choice');
+
+} else if (book === 'no'|| book==='n') {
+  //console.log('You need a second chance');
+  alert('You need a second chance');
+
+} else {
+  //console.log('Does not exist');
+  alert('Your answer not exist');
 }
 
-let SMP=prompt("Do you like twitter?(YES/NO)")
-SMP=SMP.toUpperCase();
+let SMP = prompt('Do you like twitter?(YES/NO)');
 
-if (SMP==='YES') {
-    //console.log('The most reliable Socialmedia platform');
-    alert('The most reliable Socialmedia platform');
-    
-}else if(SMP==='NO'){
-    //console.log('Try the latest update');
-    alert('Try the latest update');
+SMP = SMP.toUpperCase();
 
-}else{
-    //console.log('Does not exist');
-    alert('You should select(YES/NO)');
+if (SMP === 'YES'||SMP==='Y') {
+  //console.log('The most reliable Socialmedia platform');
+  alert('The most reliable Socialmedia platform');
+
+} else if (SMP === 'NO'||SMP==='N') {
+  //console.log('Try the latest update');
+  alert('Try the latest update');
+
+} else {
+  //console.log('Does not exist');
+  alert('You should select(YES/NO)');
 }
 
-let music=prompt('Do you like music?(y/n)');
+let music = prompt('Do you like music?(y/n)');
 
-switch (music=music.toLowerCase()) {
-    case 'y':
-        //console.log('Calms nerves');
-        alert('Calms nerves')
-    
-        break;
-    case 'n':
-    //console.log('Try to hear it');
-    alert('Try to hear it');
-        break;
+switch (music = music.toLowerCase()) {
+case 'y':
+case 'yes':
+  //console.log('Calms nerves');
+  alert('Calms nerves');
 
-    default:
-        console.log('Please check the major entry next time');
-           alert('Please check the major entry next time');
-        break;
+  break;
+case 'no':
+case 'n' :
+  //console.log('Try to hear it');
+  alert('Try to hear it');
+  break;
+
+default:
+  console.log('Please check the major entry next time');
+  alert('Please check the major entry next time');
+  break;
 }
 
-let party=prompt('Is the Conservative Party is the ruling party in Britain?(Y/N)');
-switch (party=party.toUpperCase()) {
-    case 'Y':
-        //console.log('It is the correct answer');
-        alert('It is the correct answer');
-    
-        break;
+let party = prompt('Is the Conservative Party is the ruling party in Britain?(Y/N)');
+switch (party = party.toUpperCase()) {
+case 'Y':
+case 'YES':
+  //console.log('It is the correct answer');
+  alert('It is the correct answer');
 
-        case 'N':
-            //console.log('It/s the wrong answer');
-            alert('It Is the wrong answer');
-        break;
+  break;
 
-    default:
-        //cosole.log(' It is not the labour Party');
-        alert(' It is not the labour Party');
-        break;
+case 'N':
+case 'NO':
+  //console.log('It/s the wrong answer');
+  alert('It Is the wrong answer');
+  break;
+
+default:
+  //cosole.log(' It is not the labour Party');
+  alert(' It is not the labour Party');
+  break;
 }
 
-let UNI=prompt('Is MIT University the best technical university in the world?(yes/no)');
- UNI=UNI.toLocaleLowerCase();
+let UNI = prompt('Is MIT University the best technical university in the world?(yes/no)');
+UNI = UNI.toLowerCase();
 
-if (UNI==='yes') {
-//console.log('Depending on the latest answer classification is correct');
-alert('Depending on the latest answer classification is correct');    
-}else if(UNI==='no'){
-//console.log('Depending on the latest answer classification is wrong');
-alert('Depending on the latest answer classification is wrong');
+if (UNI === 'yes' || UNI==='y') {
+  //console.log('Depending on the latest answer classification is correct');
+  alert('Depending on the latest answer classification is correct');
+} else if (UNI === 'no'|| UNI==='n') {
+  //console.log('Depending on the latest answer classification is wrong');
+  alert('Depending on the latest answer classification is wrong');
 
 }
 
 else {
-    //console.log('Review your information');
-    alert('Review your information');
+  //console.log('Review your information');
+  alert('Review your information');
 
-    
+
 }
 
 
 
-    
+
 
